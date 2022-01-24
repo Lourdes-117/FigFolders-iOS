f2f9676ce9cae3946ff9faebc2973a922897e835
Lourdes-117 <slourdesdinesh0110@outlook.com>
2022-01-25 00:57:28 +0530

    Name Regex Bugfix

--- FigFolders/Resources/StringConstants.swift
+++ FigFolders/Resources/StringConstants.swift
@@ -23,7 +23,7 @@ class StringConstants {
 ///Use Through StringConstants shared Instance
 struct RegexConstants {
     let email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    let name = "^[A-Za-z]\\w{1,29}$"
+    let name = "([a-zA-Z]{3,30}\\s*)+"
     let phoneNumber = "^(\\+\\d{1,2}\\s?)?1?\\-?\\.?\\s?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$"
     let userName = "^[a-zA-Z0-9](_(?!(\\.|_))|\\.(?!(_|\\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$"
 }
